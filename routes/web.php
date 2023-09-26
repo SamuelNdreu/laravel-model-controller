5cb3c6b108a1a2728e327d2336b41578e53786f5
SamuelNdreu <samwellsoficcial@gmail.com>
2023-09-26 15:55:46 +0200

    imported and created a new db and imported sql file

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Guest\PageController as PageController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/', [HomeController::class, "index"])->name ("home"); 
 Route::get('/', function () {
     return view('home');
 });
